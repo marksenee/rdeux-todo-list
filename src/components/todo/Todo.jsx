496d056480ec9a39b524d5b90caa7e58527c2758
marksenee <mark233891@gmail.com>
2022-08-20 16:08:12 +0900

    [Feat] 상세페이지 이동 기능

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 import IndexStyle from './css/IndexStyle';
 import ItemListStyle from './css/ItemListStyle';
 import ItemStyle from './css/ItemStyle';
 import CheckBtnStyle from './css/CheckBtnStyle';
 
+
 function Todo({ todo, onRemoveTodo, onClickCheck }) {
     const { id, title, content, isDone } = todo;
+    
+    //navigate 생성
+    const navigte = useNavigate();  
+
     return (
         <IndexStyle>
             <ItemListStyle> 
               <ItemStyle>☑ {title} : {content}</ItemStyle>
             </ItemListStyle>
             <div>
+              <CheckBtnStyle color="#f89e4a" onClick={() => {navigte("/about")}}>상세</CheckBtnStyle>
               <CheckBtnStyle color="#ff80ab" onClick={() => onRemoveTodo(id)}>삭제</CheckBtnStyle>
               {isDone ?
                 <CheckBtnStyle color="#8c9eff" onClick={() => onClickCheck(id)}>취소</CheckBtnStyle>
